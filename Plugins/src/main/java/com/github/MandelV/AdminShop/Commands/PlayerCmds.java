58753e9743042dd98d2920b60b303ff2ff416de9
Unknown <mandel.vaubourg@viacesi.fr>
2018-02-21 12:38:00 +0100

    add permission system and command implementation

--- Plugins/src/main/java/com/github/MandelV/AdminShop/Commands/PlayerCmds.java
+++ Plugins/src/main/java/com/github/MandelV/AdminShop/Commands/PlayerCmds.java
@@ -14,44 +14,88 @@ public class PlayerCmds extends Commands {
 
 
     private boolean return_cmd = false;
-    public PlayerCmds(AdminShop adminShop){
-        super(adminShop);
 
+    public PlayerCmds(AdminShop adminShop) {
+        super(adminShop);
 
     }
 
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
+
+        if (command.getName().equalsIgnoreCase("adminshop") && args.length == 0) {
+
+            this.openAdminShop(commandSender);
+
+
+        } else if (args[0].equalsIgnoreCase("listcategorie")) {
+
+            this.listCategorie(commandSender);
+
+        } else if (args[0].equalsIgnoreCase("categorie") && (args.length > 1)) {
+
+            if(args[1].equalsIgnoreCase("add")){
+
+                this.addCategorie(commandSender);
+
+            }else if(args[1].equalsIgnoreCase("remove")){
+
+                this.removeCategorie(commandSender);
+
+            }
+
+
+        }
+
+        return true;
+}
     @Override
     protected boolean command_help(CommandSender sender) {
         return false;
     }
 
+    private boolean openAdminShop(CommandSender sender){
 
-    @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
-        if (commandSender.hasPermission("adminshop.player")) {
-            if(command.getName().equalsIgnoreCase("adminshop") && args.length == 0){
-
-
-
-                adminShop.shop.open(adminShop.getServer().getPlayer(commandSender.getName()));
-
-
-            }
-            else if(args[0].equalsIgnoreCase("listcategorie")){
-
-                this.listCategorie(commandSender);
-
-            }
-
+        if(!sender.hasPermission("adminshop.open")){
+            sender.sendMessage(ChatFormatting.formatText(adminShop.getMessage().getCustomConfig().getString("permission_deny")));
+            return true;
         }
+        adminShop.shop.open(adminShop.getServer().getPlayer(sender.getName()));
+
         return true;
     }
 
-    private void listCategorie(CommandSender sender){
+    private boolean listCategorie(CommandSender sender){
+        if(!sender.hasPermission("adminshop.categorie.listcategorie")){
+            sender.sendMessage(ChatFormatting.formatText(adminShop.getMessage().getCustomConfig().getString("permission_deny")));
+            return true;
+        }
+
         sender.sendMessage(ChatFormatting.formatText("[&6AdminShop&f] &2Nombre de categorie : &6" + adminShop.categories.size()));
         adminShop.categories.forEach(cat ->{
             sender.sendMessage(ChatFormatting.formatText("&6"+ cat.getName()));
         });
+        return true;
+    }
+
+    private boolean addCategorie(CommandSender sender){
+        if(!sender.hasPermission("adminshop.categorie.add")){
+            sender.sendMessage(ChatFormatting.formatText(adminShop.getMessage().getCustomConfig().getString("permission_deny")));
+            return true;
+        }
+
+        sender.sendMessage("Ajout");
+
+        return true;
+    }
+
+    private boolean removeCategorie(CommandSender sender){
+        if(!sender.hasPermission("adminshop.categorie.remove")){
+            sender.sendMessage(ChatFormatting.formatText(adminShop.getMessage().getCustomConfig().getString("permission_deny")));
+            return true;
+        }
+        sender.sendMessage("delete");
+        return true;
     }
 
 
